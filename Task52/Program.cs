f63278cdfaa1bffaada457d9fa1a948c01bf1a82
Maria Yutaeva <mariya1982@yandex.ru>
2022-10-09 12:17:45 +0300

    сред арифм столбца

---
+++ Task52/Program.cs
@@ -0,0 +1,63 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее 
+// арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+Console.WriteLine("Укажите размерность m вашего массива:  ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Укажите размерность n вашего массива:  ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+Random random = new Random();
+int[,] numbers = new int[rows, columns];
+FillArray(numbers);
+PrintArray(numbers);
+FindAvgColumns(numbers);
+
+
+void FillArray(int[,] numbers)
+{
+    int rows = numbers.GetLength(0);
+    int columns = numbers.GetLength(1);
+    Random random = new Random();
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            numbers[i, j] = random.Next(0, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] numbers)
+{
+    int rows = numbers.GetLength(0);
+    int columns = numbers.GetLength(1);
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            Console.Write(numbers[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+void FindAvgColumns(int[,] numbers)
+{
+    
+    for (int i = 0; i < rows; i++)
+    {
+        double sum = 0;
+        double avg = 0;
+        for (int j = 0; j < columns; j++)
+            {
+            sum += numbers[j, i];
+            avg = Math.Round((sum / columns),2);
+            }
+        Console.WriteLine("Среднее арифметическое столбцов массива = " + avg);
+    }
+     
+}
